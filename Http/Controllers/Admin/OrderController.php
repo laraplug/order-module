dbd2025a312e9858369ca33bc1a65e2318498e76
왕규호 <wgh8020@nate.com>
2024-03-15 18:17:45 +0900

    [order 설정 변경] basic 일 경우 추가로 입력되도록 수정33

--- Http/Controllers/Admin/OrderController.php
+++ Http/Controllers/Admin/OrderController.php
@@ -174,17 +174,17 @@ class OrderController extends AdminBaseController
                 ]);
                 $testDump = ['items'=>[]];
                 foreach ($order as $item){
-                    $items = $order->items[0];
-                    $orderItems = $order->items;
-                    $type = $items->product->type;
+                    $items = $item->items[0];
+                    $orderItems = $item->items;
+                    $type = $item->product->type;
                     $result = [
-                            'id' => $order->id,
-                            '이름' => $order->name,
-                            '주문자명' => $order->payment_name,
+                            'id' => $item->id,
+                            '이름' => $item->name,
+                            '주문자명' => $item->payment_name,
                             '결제금액' => number_format($order->total_price),
-                            '결제수단' => $order->payment_method_id == 'direct_bank' ? '무통장 입금' : '카드',
-                            '주문상태' => $order->status->name,
-                            '주문날짜' => $order->created_at,
+                            '결제수단' => $item->payment_method_id == 'direct_bank' ? '무통장 입금' : '카드',
+                            '주문상태' => $item->status->name,
+                            '주문날짜' => $item->created_at,
                             '원아명' => $this->findValueByKey($items->option_values, 'student_name'),
                             '사이즈' => $this->findValueByKey($items->option_values, 'select-size'),
                             '원ID' => $this->findValueByKey($items->option_values, 'academy_select'),
