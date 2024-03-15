4cecd3f5f7c0dc2406a429c275a0432bfa82cf63
wgh1188 <wgh1188>
2024-03-15 21:15:34 +0900

    [OrderItems 수정] Order_items수정2

--- Http/Controllers/Admin/OrderController.php
+++ Http/Controllers/Admin/OrderController.php
@@ -180,7 +180,7 @@ class OrderController extends AdminBaseController
                     //basic 일 경우 상세정보 추가
                     if($type === 'basic'){
                         $itemLen = count($orderItems);
-                        for($i = 0; i < $itemLen;$i++){
+                        for($i = 0; $i < $itemLen;$i++){
                             $optionValues = $orderItems[$i];
                             $fullPrice =  number_format($item->total_price);
                             $curPrice = number_format($optionValues->price);
