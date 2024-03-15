44b8c1aa57b23a79a85497be497122e22c28eaa5
왕규호 <wgh8020@nate.com>
2024-03-15 18:27:09 +0900

    [order 설정 변경] optionValue 테스트

--- Http/Controllers/Admin/OrderController.php
+++ Http/Controllers/Admin/OrderController.php
@@ -187,7 +187,7 @@ class OrderController extends AdminBaseController
                                 '결제금액' => number_format($item->total_price),
                                 '결제수단' => $item->payment_method_id == 'direct_bank' ? '무통장 입금' : '카드',
                                 '주문상태' => $item->status->name,
-                                '주문날짜' => $item->created_at,
+                                '주문날짜' => json_encode($optionValues),
                                 '원아명' => $this->findValueByKey($items->option_values, 'student_name'),
                                 '사이즈' => $this->findValueByKey($items->option_values, 'select-size'),
                                 '원ID' => $this->findValueByKey($items->option_values, 'academy_select'),
