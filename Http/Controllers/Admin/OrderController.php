6037018736049a16b6fb3eacff8943e6ac3ba665
왕규호 <wgh8020@nate.com>
2024-03-15 13:53:49 +0900

    [order 설정 변경] basic 일 경우 추가로 입력되도록 수정5

--- Http/Controllers/Admin/OrderController.php
+++ Http/Controllers/Admin/OrderController.php
@@ -176,21 +176,9 @@ class OrderController extends AdminBaseController
                     $items = $order->items[0];
                     $orderItems = $order->items;
                     $type = $items->product->type;
-                    $result = [
-                        'id' => $order->id,
-                        '이름' => $order->name,
-                        '주문자명' => $order->payment_name,
-                        '결제금액' => number_format($order->total_price),
-                        '결제수단' => $order->payment_method_id == 'direct_bank' ? '무통장 입금' : '카드',
-                        '주문상태' => $order->status->name,
-                        '주문날짜' => $order->created_at,
-                        '원아명' => $this->findValueByKey($items->option_values, 'student_name'),
-                        '사이즈' => $this->findValueByKey($items->option_values, 'select-size'),
-                        '원ID' => $this->findValueByKey($items->option_values, 'academy_select'),
-//                        'itemsTest' => json_encode($itemTest)
-                    ];
+
                     if($type ==='basic' ){
-                        $result2 = $orderItems->map(function($item) use($order){
+                        $orderItems->map(function($item) use($order){
                             $itemResult = [
                                 'id' => $order->id,
                                 '이름' => $order->name,
@@ -204,11 +192,21 @@ class OrderController extends AdminBaseController
                                 '원ID' => "",
                             ];
                             return $itemResult;
-
                         });
-                        $result = array_merge($result, $result2);
-                        return $result;
                     }else {
+                        $result = [
+                            'id' => $order->id,
+                            '이름' => $order->name,
+                            '주문자명' => $order->payment_name,
+                            '결제금액' => number_format($order->total_price),
+                            '결제수단' => $order->payment_method_id == 'direct_bank' ? '무통장 입금' : '카드',
+                            '주문상태' => $order->status->name,
+                            '주문날짜' => $order->created_at,
+                            '원아명' => $this->findValueByKey($items->option_values, 'student_name'),
+                            '사이즈' => $this->findValueByKey($items->option_values, 'select-size'),
+                            '원ID' => $this->findValueByKey($items->option_values, 'academy_select'),
+//                        'itemsTest' => json_encode($itemTest)
+                        ];
                         return $result;
                     }
 
