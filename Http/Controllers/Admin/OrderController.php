f21c31d7c5755a83be98a9c5c140c4236e75864f
왕규호 <wgh8020@nate.com>
2024-03-28 14:32:06 +0900

    [OrderError 설정] option 점검2

--- Http/Controllers/Admin/OrderController.php
+++ Http/Controllers/Admin/OrderController.php
@@ -241,7 +241,7 @@ class OrderController extends AdminBaseController
                 var_dump($exportExcel);
                 $sheet->fromArray($exportExcel,null,'A3');
             });
-        })
+        });
 //            ->download('xlsx');
 //        openedWindow.close();
     }
