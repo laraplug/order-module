699148d8da020f87b96e8cb30f93b6b0afd2d02a
왕규호 <wgh8020@nate.com>
2024-05-02 18:18:51 +0900

    [OrderError 설정] EnreUtilityMall 계정 수정8

--- Http/Controllers/Admin/OrderController.php
+++ Http/Controllers/Admin/OrderController.php
@@ -182,6 +182,7 @@ class OrderController extends AdminBaseController
                 $exportExcel = [];
                 foreach ($order as $item){
                     $orderItems = $item->items;
+                    var_dump(count($orderItems));
                     if(count($orderItems)>1){
                     $items = $item->items[0];
 
@@ -255,8 +256,8 @@ class OrderController extends AdminBaseController
 //                var_dump($exportExcel);
                 $sheet->fromArray($exportExcel,null,'A3');
             });
-        })
-            ->download('xlsx');
+        });
+//            ->download('xlsx');
 //        openedWindow.close();
     }
 }
